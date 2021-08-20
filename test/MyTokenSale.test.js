8c0098e75b9731715e92507848f7b11c50fc53da
Scott Millner <scottmillner237@gmail.com>
2021-08-20 07:56:56 -0600

    add more tests for MyTokenSale.test.js

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -8,7 +8,7 @@ const expect = chai.expect;
 require('dotenv').config({ path: '../.env' });
 
 // TODO: break out asserts into individual tests
-contract('TokenSale Test', async (accounts) => {
+contract('TokenSale', async (accounts) => {
 	// Arrange
 	const [deployerAccount, recipient, anotherAccount] = accounts;
 
@@ -18,5 +18,26 @@ contract('TokenSale Test', async (accounts) => {
 
 		// Assert
 		await expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(new BN(0));
+	});
+
+	it('should have all tokens in the contract by default', async () => {
+		// Arrange
+		const instance = await Token.deployed();
+		const balanceOfTokenSale = await instance.balanceOf(TokenSale.address);
+		const totalSupply = await instance.totalSupply();
+
+		// Assert
+		expect(balanceOfTokenSale).to.be.a.bignumber.equal(totalSupply);
+	});
+
+	it('should be possible to buy tokens', async () => {
+		// Arrange
+		const tokenInstance = await Token.deployed();
+		const tokenSaleInstance = await TokenSale.deployed();
+		const balanceBefore = await tokenInstance.balanceOf(deployerAccount);
+
+		// Act/Assert
+		await expect(tokenSaleInstance.sendTransaction({ from: deployerAccount, value: web3.utils.toWei('1', 'wei') })).to.be.fulfilled;
+		await expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceBefore.add(new BN(1)));
 	});
 });
